09e5aeee98a7819f5be1e95b1e6a96cec7e0fd55
johnpiapian <johnpiapian@gmail.com>
2025-11-08 03:09:41 -0600

    fix: backend

--- backend/config/default.ts
+++ backend/config/default.ts
@@ -1,7 +1,7 @@
-/**
- * Create .env.development to override these values during development.
- * Create .env.production to override these values during production.
- */
+import dotenv from 'dotenv'
+
+// create .env file to override default config values
+dotenv.config({ path: [`./config/.env.${process.env.NODE_ENV}`, './config/.env'] })
 
 export default {
   PORT: process.env.PORT || 3000,
